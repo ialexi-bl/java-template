804094c6f7d98ac30c376b516c15bd6b096dc416
Aleksei Chashin <jabuj@yandex-team.ru>
2021-09-16 15:41:36 +0300

    Added sort algorithm

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,11 +8,47 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+  private static int[] merge(int[] array, int start, int middle, int end) {
+    int[] result = new int[end - start];
+
+    int p1 = start, p2 = middle, p = 0;
+    while (p1 < middle && p2 < end) {
+      if (array[p1] < array[p2]) {
+        result[p++] = array[p1++];
+      } else {
+        result[p++] = array[p2++];
+      }
+    }
+    while (p1 < middle) {
+      result[p++] = array[p1++];
+    }
+    while (p2 < end) {
+      result[p++] = array[p2++];
+    }
+
+    return result;
   }
 
-  public static void sort (List<Integer> list) {
+  private static void sort(int[] array, int start, int end) {
+    if (end - start < 2) {
+      return;
+    }
+
+    int middle = start + (end - start) / 2;
+    sort(array, start, middle);
+    sort(array, middle, end);
+
+    int[] merged = merge(array, start, middle, end);
+    for (int i = 0, m = merged.length; i < m; i++) {
+      array[start + i] = merged[i];
+    }
+  }
+
+  public static void sort(int[] array) {
+    sort(array, 0, array.length);
+  }
+
+  public static void sort(List<Integer> list) {
     Collections.sort(list);
   }
 }
